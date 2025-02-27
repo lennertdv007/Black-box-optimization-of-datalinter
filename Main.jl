b892ab4a414e0c9782d00ad2ae3f0f51997dc569
lennertdv007 <lennert.william.k.de.ville@vub.be>
2025-02-27 15:18:52 +0100

    Added fitness functions

--- Main.jl
+++ Main.jl
@@ -1,5 +1,5 @@
 using Pkg
-using DataFrames
+#using DataFrames
 Pkg.activate("/Users/lennertdeville/Desktop/vub/3e_bach/bachelorproef/DataLinter")
 Pkg.instantiate()
 
@@ -38,18 +38,13 @@ function count_lint_error(output)
     for ((linter, _), result) in output
         lname = String(linter.name)
         if !haskey(result_dict, lname)
-            result_dict[lname] = Dict("true" => 0, "false" => 0, "nothing" => 0, "info" => 0, "warning" => 0, "important" => 0, "experimental" => 0) # Initialize dict key with all 0 values
+            result_dict[lname] = Dict("info" => 0, "warning" => 0, "important" => 0, "experimental" => 0) # Initialize dict key with all 0 values
         end
         if isnothing(result)
-            result_dict[lname]["nothing"] += 1
         elseif result
-            result_dict[lname]["true"] += 1
             result_dict[lname][linter.warn_level] += 1
-        else
-            result_dict[lname]["false"] += 1
         end
     end
-    #println("Error count: ", result_dict)
     return result_dict
 end
 
@@ -64,12 +59,11 @@ const WARN_LEVEL_TO_NUM = Dict("info" => 1,
 """
 Calculates the score of a lint error based on the amount of errors and the warning level
 """
-function score_lint_error(lint_error)
+function score_lint_error(lint_out)
     score = 0
-    for (linter, error_dict) in lint_error
-        for (error_type, count) in error_dict
-            score += get(WARN_LEVEL_TO_NUM, error_type, 0) * count
-            #score += WARN_LEVEL_TO_NUM[error_type] * count
+    for ((linter, _), result) in lint_out
+        if !isnothing(result)
+            score += get(WARN_LEVEL_TO_NUM, linter.warn_level, 0)
         end
     end
     return score
@@ -86,15 +80,12 @@ function run_datalinter(conf)
     out = lint(ctx, kb; config=conf, progress=false)
     elapsed_time = time() - start_time
     print("\n\n\n\n")
-    #process_output(out; buffer=stdout, show_passing=true, show_stats=true, show_na=true)
-    #print("\n\n\n\n")
-    #print("out: ", out)
-    #print("\n\n\n\n")
     println("Confguration took ", round(elapsed_time, digits=4), " seconds.")
     println("In that time, ", length(out), " problems were found.")
     lint_error = count_lint_error(out)
     print("\n\n\n\n")
-    s = score_lint_error(lint_error)
+    s = score_lint_error(out)
+    println("lint_error", lint_error)
     println("Score: ", s)
     print("\n\n\n\n")
     return s
@@ -102,6 +93,7 @@ end
 
 """
 function that maps the paramaters to a config for bboptimize
+    current version has only parameters to enable or disable linters
 """
 function params_to_config(params)
     config = Dict{String,Any}( # Initialize config empty
@@ -116,7 +108,8 @@ end
 
 function main()
 
-    function objective(params)
+    function objective(params, time_weight=0.5)
+        println("params: ", params)
         # 1) construct a config for the datalinter which is a temp toml file 
         config = params_to_config(params)
         # 2) run the linter with the config
@@ -124,16 +117,29 @@ function main()
         ctx = DataLinter.DataInterface.build_data_context(FILEPATH)
         start_time = time()
         out = lint(ctx, kb; config=config, progress=false)
-        # 2.2) delete config
         # 3) calculate the data error indicator and the timing
         elapsed_time = time() - start_time
-        lint_error = count_lint_error(out)
+        s = score_lint_error(out)
+        # score for linters that found nothing = 0, so this has no influence on the fitness function
+        # maybe we should add a penalty for linters that found nothing? (score -= 1)?
         # 4) calculate the value of the fitness function (function that penalizes long timings and low errors)
-        s = score_lint_error(lint_error)
-        # 5 return the value of the fitness function
-        return float(1 / s) + elapsed_time #ERROR: LoadError: ArgumentError: The supplied fitness function does NOT return the expected fitness type Float64 when called with a potential solution it returned 96 of type Int64 so we cannot optimize it!
-    end # 1/s because bboptimize looks to minimize the fitness function
+        # bboptimize tries to minimize the fitness function
 
+        fitness::Float64 = (elapsed_time * time_weight) + 1 / s
+        normalized_fitness = (fitness - 1) / fitness
+        # does the time_weight make sense?
+        # does the fitness function need to be normalized?
+
+        #fitness::Float64 = 1 / s
+        # problem with this fitness function that it will just enable all the linters that have a score > 0
+
+        # 5 return the value of the fitness function
+        return normalized_fitness
+    end
+
+    # How can we use discrete values for the searchrange?
+    # If not possible can we use a step size for the parameters?
+    # If not possible can we use a different optimizer?
     searchrange = [
         (0, 1),
         (0, 1),
@@ -151,12 +157,16 @@ function main()
         (0, 1),
         (0, 1)]
 
-    res = bboptimize(objective; searchrange=searchrange, NumDimensions=length(searchrange), MaxSteps=700)
+
+    res = bboptimize(objective; searchrange=searchrange, NumDimensions=length(searchrange), StepRange=0.1, MaxSteps=1000)
 
     bs = best_candidate(res)
     bf = best_fitness(res)
-    println("best candidate: ", bs, " with fitness: ", bf, " = ", 1 / bf)
+    println("best candidate: ", bs, " with fitness: ", bf)
     println("best config: ", print_config(params_to_config(bs)))
+
+    enable_all_param = [1 for i in 1:length(LINTERS)]
+    x = run_datalinter(params_to_config(enable_all_param))
 end
 main()
 
